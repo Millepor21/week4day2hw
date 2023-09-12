752f3863fd8adbd6a5d987b87b9505e545ec7ed2
Millepor21 <millepor21@gmail.com>
2023-09-12 18:36:20 -0400

    forgot the time complexity

--- ex3.py
+++ ex3.py
@@ -1,10 +1,10 @@
 nums_list = [10,23,45,70,11,15]
 target = 70
-
-def num_search(alist,atarget):
-    for i, num in enumerate(alist):
-        if num == atarget:
-            return f'Your number is in position {i + 1}'
-    return -1
+#O(N) + O(4*1) => O(N)
+def num_search(alist,atarget): #O(1)
+    for i, num in enumerate(alist): #O(N)
+        if num == atarget: #O(1)
+            return f'Your number is in position {i + 1}' #O(1)
+    return -1 #O(1)
 # If number is not present return -1
 num_search(nums_list, target)
